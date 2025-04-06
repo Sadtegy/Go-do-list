2c961288eccf1d4911204bae6e8343e25103a418
Sadtegy <shakiul017@gmail.com>
2025-04-06 11:06:54 +0600

    ...

---
+++ to-do-list/main.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main() {
+	var instal = "Install Go and set up your workspace"
+	var basics = "learn Go basics variables, data type, function"
+	var practice = "Practice control flow: if, switch, loop"
+	var understand = "Understand arrays, slice and map"
+	var cli = "make small ClI project like to-do-list"
+
+	var todoIteam = []string{instal, basics, practice, understand, cli}
+
+	fmt.Println("Welcome to the Go-Do-List")
+	fmt.Println("")
+
+	fmt.Println("Things you need to do for lean go")
+
+	for index, task := range todoIteam {
+		fmt.Printf("%d. %s/n", index+1, task)
+	}
+
+}
