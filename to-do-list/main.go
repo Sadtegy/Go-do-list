b971c1eef0ffa29cd2934b78a0d52a30b2a69b12
Sadtegy <shakiul017@gmail.com>
2025-04-06 11:48:17 +0600

    .....

--- to-do-list/main.go
+++ to-do-list/main.go
@@ -8,8 +8,12 @@ func main() {
 	var practice = "Practice control flow: if, switch, loop"
 	var understand = "Understand arrays, slice and map"
 	var cli = "make small ClI project like to-do-list"
-
 	var todoIteam = []string{instal, basics, practice, understand, cli}
+
+	printTask(todoIteam)
+}
+
+func printTask(todoIteam []string) {
 
 	fmt.Println("Welcome to the Go-Do-List")
 	fmt.Println("")
@@ -17,7 +21,7 @@ func main() {
 	fmt.Println("Things you need to do for lean go")
 
 	for index, task := range todoIteam {
-		fmt.Printf("%d. %s/n", index+1, task)
+		fmt.Printf("%d. %s \n", index+1, task)
 	}
 
 }
