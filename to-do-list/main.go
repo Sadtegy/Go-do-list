f9fe478ff181b8c4affe8cd03de003c23d8c1492
Sadtegy <shakiul017@gmail.com>
2025-04-07 13:12:16 +0600

    ....

--- to-do-list/main.go
+++ to-do-list/main.go
@@ -3,25 +3,28 @@ package main
 import "fmt"
 
 func main() {
-	var instal = "Install Go and set up your workspace"
-	var basics = "learn Go basics variables, data type, function"
-	var practice = "Practice control flow: if, switch, loop"
-	var understand = "Understand arrays, slice and map"
-	var cli = "make small ClI project like to-do-list"
-	var todoIteam = []string{instal, basics, practice, understand, cli}
+	var install = "install go language from the website"
+	var setup = "setup go and code editor in your pc"
+	var basics = "learn go basic fundamentals"
+	var create = "create some small project by using go"
+	var TaskLists = []string{install, setup, basics, create}
 
-	printTask(todoIteam)
+	printTask(TaskLists)
+	fmt.Println("")
+	addTask(TaskLists, "have a cup of coffee")
 }
 
-func printTask(todoIteam []string) {
-
-	fmt.Println("Welcome to the Go-Do-List")
+func printTask(TaskLists []string) {
+	fmt.Println("Welcome to my GO TO LIST")
 	fmt.Println("")
+	fmt.Println("list of tasks")
 
-	fmt.Println("Things you need to do for lean go")
-
-	for index, task := range todoIteam {
+	for index, task := range TaskLists {
 		fmt.Printf("%d. %s \n", index+1, task)
 	}
+}
 
+func addTask(TaskLists []string, newTask string) {
+	var updatedTaskItem = append(TaskLists, newTask)
+	printTask(updatedTaskItem)
 }
