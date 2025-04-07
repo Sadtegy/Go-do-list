b70a871d3840884e99a14bf4b7015bff9c729592
Sadtegy <shakiul017@gmail.com>
2025-04-07 17:26:37 +0600

    ....

--- to-do-list/main.go
+++ to-do-list/main.go
@@ -3,28 +3,29 @@ package main
 import "fmt"
 
 func main() {
-	var install = "install go language from the website"
-	var setup = "setup go and code editor in your pc"
-	var basics = "learn go basic fundamentals"
-	var create = "create some small project by using go"
-	var TaskLists = []string{install, setup, basics, create}
+	var install = "Install go language from the website"
+	var setup = "Setup go and code editor in your pc"
+	var basics = "Learn go basic fundamentals"
+	var create = "Create some small project by using go"
+	var taskLists = []string{install, setup, basics, create}
 
-	printTask(TaskLists)
+	taskLists = addTask(taskLists, "Have a cup of coffee")
 	fmt.Println("")
-	addTask(TaskLists, "have a cup of coffee")
+
+	printTask(taskLists)
 }
 
-func printTask(TaskLists []string) {
-	fmt.Println("Welcome to my GO TO LIST")
+func printTask(taskLists []string) {
+	fmt.Println("Welcome to the GO TO LIST")
 	fmt.Println("")
-	fmt.Println("list of tasks")
+	fmt.Println("List of Todos")
 
-	for index, task := range TaskLists {
-		fmt.Printf("%d. %s \n", index+1, task)
+	for index, task := range taskLists {
+		fmt.Printf("%d. %s \n", index, task)
 	}
 }
 
-func addTask(TaskLists []string, newTask string) {
-	var updatedTaskItem = append(TaskLists, newTask)
-	printTask(updatedTaskItem)
+func addTask(taskLists []string, newList string) []string {
+	var updatedTaskItem = append(taskLists, newList)
+	return updatedTaskItem
 }
